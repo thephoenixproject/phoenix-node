36fc7185d4a2b42b215e4c606cc8a7a4a507fec2
aconfino <aconfino@gmail.com>
2014-03-21 17:22:56 -0400

    FRIDAY 2nd!

--- modules/nexus/manifests/init.pp
+++ modules/nexus/manifests/init.pp
@@ -1,19 +1,8 @@
 class nexus {
-	
-  class{ 'nexus::package':
-    notify => Class['nexus::service']
-  }
   contain 'nexus::package'
-  
-  class{ 'nexus::config':
-    require => Class['nexus::package'],
-    notify => Class['nexus::service'],
-  }
   contain 'nexus::config'
-	
-  class{ 'nexus::service':
-    require => Class['nexus::config'],
-  }
   contain 'nexus::service'
+
+  Class['nexus::package'] -> Class['nexus::config'] -> Class['nexus::service']
 
 }
