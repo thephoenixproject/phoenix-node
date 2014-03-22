529e2bb7c9ad63e01b5f5068f1bb9f7625683d83
aconfino <aconfino@gmail.com>
2014-03-21 20:19:04 -0400

    start stop

--- modules/nexus/manifests/service.pp
+++ modules/nexus/manifests/service.pp
@@ -1,10 +1,11 @@
 class nexus::service {
-	
   service { 'nexus':
     ensure => running,
     enable => true,
+    restart => "/sbin/service nexus restart",
+    start => "/sbin/service nexus start",
+    status => "/sbin/service nexus status",
+    stop => "/sbin/service nexus stop",
   }
-
-
 
 }
