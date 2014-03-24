d56a13bd989f91abfba50a2d13a972bf87b28c46
aconfino <aconfino@gmail.com>
2014-03-24 12:35:15 -0400

    wrapper

--- modules/nexus/manifests/config.pp
+++ modules/nexus/manifests/config.pp
@@ -1,12 +1,10 @@
 class nexus::config {
 	
-  #Sonatype startup script
-  file { '/usr/local/nexus/bin/nexus':
+  file { '/usr/local/nexus-2.7.2-03/bin/nexus':
     source => 'puppet:///modules/nexus/nexus',
     mode    => '0755',
   }
 
-  #VGI wrapper script
   file { '/etc/init.d/nexus':
     source => 'puppet:///modules/nexus/nexus-wrapper',
     mode    => '0755',
@@ -22,21 +20,21 @@ class nexus::config {
 	recurse => true,
 	owner => "nexus",
 	group => "nexus",
-	require => User["nexus"]
+	require => User["nexus"],
   }
   
   file { "/usr/local/nexus-2.7.2-03":
 	recurse => true,
 	owner => "nexus",
 	group => "nexus",
-	require => User["nexus"]
+	require => User["nexus"],
   }
   
   file { "/usr/local/sonatype-work":
 	recurse => true,
 	owner => "nexus",
 	group => "nexus",
-	require => User["nexus"]
+	require => User["nexus"],
   }
   
 }	
