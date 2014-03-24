24cf40c9732afca6b8557770fb3a1339a70f2029
aconfino <aconfino@gmail.com>
2014-03-24 16:13:46 -0400

    commit

--- modules/nexus/manifests/params.pp
+++ modules/nexus/manifests/params.pp
@@ -2,7 +2,7 @@ class nexus::params {
 
   $nexus_home_dir = '/usr/local/nexus'
   $nexus_host = '0.0.0.0'
-  $nexus_port = '6000'
+  $nexus_port = '5001'
   $nexus_user = 'nexus'
   $nexus_group = 'nexus'
 	
