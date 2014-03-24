4a3a7bcd51ac79a54ebda5d170110e1f2f99b102
aconfino <aconfino@gmail.com>
2014-03-24 15:43:50 -0400

    all mine...

--- modules/nexus/manifests/init.pp
+++ modules/nexus/manifests/init.pp
@@ -23,6 +23,8 @@ class nexus (
   contain 'nexus::package'
   contain 'nexus::config'
   contain 'nexus::service'
+  
+  Class['nexus::package']
 
-  Class['nexus::package'] -> Class['nexus::config'] -> Class['nexus::service']
+#Class['nexus::package'] -> Class['nexus::config'] -> Class['nexus::service']
 }
