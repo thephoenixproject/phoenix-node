e34f03af55820c9e0f510367b3ae419d74406f47
willman <cewillm@gmail.com>
2014-03-24 11:55:39 -0400

    bugfix

--- modules/maven/manifests/init.pp
+++ modules/maven/manifests/init.pp
@@ -1,7 +1,7 @@
 class maven (
-  $version           => $maven::params::version,
-  $maven_home_dir    => $maven::params::maven_home_dir,
-  $download_base_url => $maven::params::download_base_url,
+  $version           = $maven::params::version,
+  $maven_home_dir    = $maven::params::maven_home_dir,
+  $download_base_url = $maven::params::download_base_url,
 ) {
   maven::package {
     version            => $verison,
