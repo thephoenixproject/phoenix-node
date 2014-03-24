c2cb35e7e83773bbc6d9933a47e5f3635dab9aae
aconfino <aconfino@gmail.com>
2014-03-24 15:03:52 -0400

    config

--- modules/nexus/manifests/init.pp
+++ modules/nexus/manifests/init.pp
@@ -1,10 +1,21 @@
-class nexus {
-
+class nexus (
+  $nexus_home_dir = $nexus::params::nexus_home_dir,
+  $nexus_user     = $nexus::params::nexus_user,
+  $nexus_group    = $nexus::params::nexus_group,
+  $nexus_host     = $nexus::params::nexus_host,
+  $nexus_port     = $nexus::params::nexus_port,
+  ) inherits nexus::params {
   include stdlib
 	  
   contain 'nexus::package'
   contain 'nexus::config'
   contain 'nexus::service'
+  
+  class{ 'nexus::config':
+    nexus_home_dir => $nexus_home_dir,
+    nexus_host     => $nexus_host,
+    nexus_port     => $nexus_port,
+  }
 
   Class['nexus::package'] -> Class['nexus::config'] -> Class['nexus::service']
 }
