fec7c1e25e9f7e3ec2df13293a47fa8d27fcf3dc
aconfino <aconfino@gmail.com>
2014-03-25 14:49:35 -0400

    config

--- modules/jenkins/manifests/params.pp
+++ modules/jenkins/manifests/params.pp
@@ -1,6 +1,6 @@
 class jenkins::params {
 
   $jenkins_home = '/var/lib/jenkins'
-  $base_plugin_url = 'https://updates.jenkins-ci.org/download/plugins/'
+  $base_plugin_url = 'https://updates.jenkins-ci.org/download/plugins'
 
 }
