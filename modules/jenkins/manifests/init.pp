f2e1d9c466c06e66dd781c5115de4e118b34460a
aconfino <aconfino@gmail.com>
2014-03-25 10:12:17 -0400

    plugin

--- modules/jenkins/manifests/init.pp
+++ modules/jenkins/manifests/init.pp
@@ -2,11 +2,13 @@ class jenkins {
   contain 'jenkins::package'
   contain 'jenkins::config'
   contain 'jenkins::service'
-
-  Class['jenkins::package'] -> Class['jenkins::config'] -> Class['jenkins::service']
-
+  
   plugin {'email-ext':
       plugin_name => 'email-ext',
       version => '2.37.1',
- }
+  }
+
+  Class['jenkins::package'] -> Class['jenkins::config'] -> Class['jenkins::service']
+
+  
 }
