04343f31f3af38a1b51e31d70d16523fa735546c
aconfino <aconfino@gmail.com>
2014-03-25 13:43:52 -0400

    config

--- manifests/site-ac.pp
+++ manifests/site-ac.pp
@@ -16,7 +16,7 @@ include jenkins
 #include sonar
 include nexus
 
-jenkins::plugin inherits jenkins::params {'email-ext' :
+jenkins::plugin { 'email-ext' :
   base_url => $jenkins::params::base_plugin_url,
   plugin_name => 'email-ext',
   version => '2.37.1',
