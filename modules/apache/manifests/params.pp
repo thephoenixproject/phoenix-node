81f4828307a0c813dfb27fa7d80355db53a1f4c6
willman <cewillm@gmail.com>
2014-03-26 12:14:53 -0400

    oops

--- modules/apache/manifests/params.pp
+++ modules/apache/manifests/params.pp
@@ -1,7 +1,7 @@
 # Class: apache::params
 #
 # Manages parameters for the Apache WWW installation
-class apache::parameters {
+class apache::params {
   $package            = 'httpd'
   $www_root_directory = '/opt/apache/www'
 }
