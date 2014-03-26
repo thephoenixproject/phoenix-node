45e34609e24de9bf9e54c84bf8a7fb812d4d8f28
willman <cewillm@gmail.com>
2014-03-26 13:58:44 -0400

    oops

--- modules/apache/manifests/service.pp
+++ modules/apache/manifests/service.pp
@@ -3,9 +3,9 @@
 # Manages the Apache WWW service
 class apache::service () inherits apache::params {
   if ($apache::disabled or $apache::absent) {
-    $ensure => 'stopped'
+    $ensure = 'stopped'
   } else {
-    $ensure => 'running'
+    $ensure = 'running'
   }
 
   service { $apache::params::package:
