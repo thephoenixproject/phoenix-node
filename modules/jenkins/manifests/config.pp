6fff01b1d58db722bfc295572496a2de183024e9
aconfino <root@phoenix-node-1.local>
2014-03-26 21:25:07 +0000

    wrote provide and type to handle JDK

--- modules/jenkins/manifests/config.pp
+++ modules/jenkins/manifests/config.pp
@@ -5,4 +5,19 @@ class jenkins::config {
     source => 'puppet:///modules/jenkins/jenkins',
   }
 
+ class { "jdk_config":
+
+  }
+
+  jenkins_jdk { "JDK 1.7.0" :
+    java_home => "/usr/java/jdk1.7.0_51",
+    config_file => "$jenkins::jenkins_home/config.xml",
+  }
+
+
+  jenkins_jdk { "JDK7" :
+    java_home => "/usr/lib/jvm/java-7-openjdk-amd64",
+    config_file => "$jenkins::jenkins_home/config.xml",
+    ensure => absent,
+  }
 }
