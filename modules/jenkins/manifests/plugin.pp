0f9e7caff0824de6fcfc91f3a91d0c502bab9b28
aconfino <root@phoenix-node-1.local>
2014-03-27 14:59:34 +0000

    added force option

--- modules/jenkins/manifests/plugin.pp
+++ modules/jenkins/manifests/plugin.pp
@@ -8,16 +8,18 @@ define jenkins::plugin(
   $version,
   $extension = 'hpi',
   $ispinned = false,
+  $forceupdate = false,
   ) {
  
   $plugin_filename = "${plugin_name}.${extension}"
   $plugin_url = "${base_url}/${plugin_name}/${version}/${plugin_filename}"
   $plugin_home = "$jenkins::jenkins_home/plugins"
-  
+
   netinstall::wget { "${plugin_name}" :
 	destination => "${plugin_home}/${plugin_filename}",
 	url => $plugin_url,
 	args => "",
+        forceupdate => $forceupdate,
         notify => Service['jenkins'],
   }
 
