a3e38bc951a3f014562483b9ec60e21e3f1113f6
willman <cewillm@gmail.com>
2014-03-27 11:17:12 -0400

    svn module

---
+++ modules/apache/manifests/modules/mod_subversion/anonymous.pp
@@ -0,0 +1,21 @@
+# Define: apache::modules::mod_subversion::anonymous
+#
+# Enables mod_svn with full anonymous read/write access
+# to a provided location.
+define apache::modules::mod_subversion::anonymous () {
+  include apache::params
+
+  $conf_path = "${apache::params::conf_d_directory}/${name}.conf"
+
+  package { 'mod_dav_svn':
+    ensure  => $apache::ensure,
+    require => Class['apache::package'],
+  }
+
+  file { $conf_path:
+    ensure  => $apache::ensure,
+    content => template('puppet:://apache/mod_subversion_anonymous_conf.erb'),
+    require => Class['apache::settings'],
+    notify  => Class['apache::service'],
+  }
+}
